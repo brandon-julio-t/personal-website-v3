da26da483e19030add431efc4382296d39b727c8
brandon-julio-t <brandon-julio-t@users.noreply.github.com>
2020-12-20 10:14:57 +0000

    Prettified Code!

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,52 +1,52 @@
 require("dotenv").config({
   path: `.env.${process.env.NODE_ENV}`,
-})
+});
 
 module.exports = {
-    siteMetadata: {
-        title: `Personal Website`,
-        description: `Personal website with Gatsby.js`,
-        author: `@brandon_julio_t`,
-        siteUrl: `https://www.brandonjuliothenaro.my.id`,
+  siteMetadata: {
+    title: `Personal Website`,
+    description: `Personal website with Gatsby.js`,
+    author: `@brandon_julio_t`,
+    siteUrl: `https://www.brandonjuliothenaro.my.id`,
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "UA-142762819-1",
+      },
     },
-    plugins: [
-        {
-            resolve: "gatsby-plugin-google-analytics",
-            options: {
-                trackingId: "UA-142762819-1",
-            },
-        },
-        "gatsby-plugin-sharp",
-        "gatsby-plugin-react-helmet",
-        "gatsby-plugin-sitemap",
-        "gatsby-plugin-offline",
-        "gatsby-plugin-postcss",
-        {
-            resolve: "gatsby-plugin-manifest",
-            options: {
-                icon: "src/images/icon.jpg",
-            },
-        },
-        "gatsby-transformer-sharp",
-        {
-            resolve: "gatsby-source-filesystem",
-            options: {
-                name: "images",
-                path: "./src/images/",
-            },
-            __key: "images",
-        },
-        {
-            resolve: `gatsby-plugin-layout`,
-            options: {
-                component: require.resolve(`./src/components/layouts/index.tsx`),
-            },
-        },
-        {
-            resolve: `gatsby-source-github-api`,
-            options: {
-                token: process.env.GITHUB_GRAPHQL_TOKEN,
-                graphQLQuery: `{
+    "gatsby-plugin-sharp",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    "gatsby-plugin-offline",
+    "gatsby-plugin-postcss",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.jpg",
+      },
+    },
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    {
+      resolve: `gatsby-plugin-layout`,
+      options: {
+        component: require.resolve(`./src/components/layouts/index.tsx`),
+      },
+    },
+    {
+      resolve: `gatsby-source-github-api`,
+      options: {
+        token: process.env.GITHUB_GRAPHQL_TOKEN,
+        graphQLQuery: `{
                     viewer {
                         pinnedItems(first: 6) {
                         nodes {
@@ -66,7 +66,7 @@ module.exports = {
                         }
                     }
                 }`,
-            }
-        }
-    ],
+      },
+    },
+  ],
 };
