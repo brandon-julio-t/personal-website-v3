9b5c19bb273f09dfae41823c4a47bb7013886b35
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2020-12-26 16:32:14 +0700

    added 20-2 photo, don't know why it was removed

---
+++ src/components/pages/index/slc.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import ExternalLink from '../../external-link';
+import Angkatan from '../../images/angkatan';
+
+export default function SLC() {
+  return (
+    <section>
+      <h2 className="text-5xl text-center">
+        <ExternalLink href="https://www.instagram.com/slcbinusuniv/" className="hover:underline">
+          Software Laboratory Center
+        </ExternalLink>{' '}
+        (20-2)
+      </h2>
+
+      <figure className="max-w-screen-md mx-auto my-8 transform-gpu transition duration-300 ease-in-out hover:scale-105">
+        <ExternalLink href="https://www.instagram.com/slcbinusuniv">
+          <Angkatan alt="20-2 assistants at Software Laboratory Center" className="rounded-3xl" />
+        </ExternalLink>
+
+        <figcaption className="text-xl text-center italic text-gray-500 my-8">
+          — “In Honesty and Hardwork, We Strive for Excellence” —
+        </figcaption>
+      </figure>
+    </section>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,17 @@
-import * as React from "react";
+import * as React from 'react';
 
-import Certificate from "../components/pages/index/certificate";
-import Hero from "../components/pages/index/hero";
-import TechnologyStack from "../components/pages/index/technology-stack";
-import GithubProjects from "../components/pages/index/github-projects";
+import Certificate from '../components/pages/index/certificate';
+import Hero from '../components/pages/index/hero';
+import TechnologyStack from '../components/pages/index/technology-stack';
+import GithubProjects from '../components/pages/index/github-projects';
+import SLC from '../components/pages/index/slc';
 
 export default function Index() {
   return (
     <main>
       <Hero />
       <GithubProjects />
+      <SLC />
       <TechnologyStack />
       <Certificate />
     </main>
