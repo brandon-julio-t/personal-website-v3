7a9466e7678ff9e9281a5d2040712318f9daa7a2
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-02-16 21:25:16 +0700

    feat: some margin

--- src/components/pages/index/github-projects.tsx
+++ src/components/pages/index/github-projects.tsx
@@ -31,39 +31,37 @@ const GithubProjects: React.FunctionComponent = () => {
   const latestRepositories = data?.data?.viewer?.repositories?.edges?.map(edge => edge.node) ?? [];
 
   return (
-    <>
-      <section>
-        <h2 className="text-center text-5xl">GitHub Projects</h2>
+    <section className="mb-8">
+      <h2 className="text-center text-5xl">GitHub Projects</h2>
 
-        {error ? (
-          <Card className="mx-auto my-8 max-w-screen-md">
-            <h3 className="mb-2 text-center text-3xl">Error</h3>
-            <p className="mb-4 text-center font-bold">Please contact the developer with the following detail</p>
-            <p className="text-center">{error}</p>
-          </Card>
-        ) : (
-          <>
-            <h3 className="mt-8 mb-4 text-center text-3xl">Pinned Repositories</h3>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-              {isLoading
-                ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
-                : pinnedRepositories.map((repository, idx) => (
-                    <GithubProjectsRepositoryCard key={idx} repository={repository} />
-                  ))}
-            </div>
+      {error ? (
+        <Card className="mx-auto my-8 max-w-screen-md">
+          <h3 className="mb-2 text-center text-3xl">Error</h3>
+          <p className="mb-4 text-center font-bold">Please contact the developer with the following detail</p>
+          <p className="text-center">{error}</p>
+        </Card>
+      ) : (
+        <>
+          <h3 className="mt-8 mb-4 text-center text-3xl">Pinned Repositories</h3>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+            {isLoading
+              ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
+              : pinnedRepositories.map((repository, idx) => (
+                  <GithubProjectsRepositoryCard key={idx} repository={repository} />
+                ))}
+          </div>
 
-            <h3 className="mt-8 mb-4 text-center text-3xl">Latest Repositories</h3>
-            <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
-              {isLoading
-                ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
-                : latestRepositories.map((repository, idx) => (
-                    <GithubProjectsRepositoryCard key={idx} repository={repository} />
-                  ))}
-            </div>
-          </>
-        )}
-      </section>
-    </>
+          <h3 className="mt-8 mb-4 text-center text-3xl">Latest Repositories</h3>
+          <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
+            {isLoading
+              ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
+              : latestRepositories.map((repository, idx) => (
+                  <GithubProjectsRepositoryCard key={idx} repository={repository} />
+                ))}
+          </div>
+        </>
+      )}
+    </section>
   );
 };
 
