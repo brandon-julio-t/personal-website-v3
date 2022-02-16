adaf37c55cac3e7a2909c59273df6b873761597b
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-02-16 21:13:33 +0700

    refactor: some typings

--- src/components/pages/index/github-projects.tsx
+++ src/components/pages/index/github-projects.tsx
@@ -1,9 +1,9 @@
-import * as React from "react";
-import useSWR, { Fetcher } from "swr";
-import IRepository from "../../../interfaces/repository";
-import Card from "../../common/card";
-import Skeleton from "../../common/skeleton";
-import GithubProjectsRepositoryCard from "./github-projects-repository-card";
+import * as React from 'react';
+import useSWR, { Fetcher } from 'swr';
+import IRepository from '../../../interfaces/repository';
+import Card from '../../common/card';
+import Skeleton from '../../common/skeleton';
+import GithubProjectsRepositoryCard from './github-projects-repository-card';
 
 interface IData {
   viewer: {
@@ -18,18 +18,17 @@ interface IData {
   };
 }
 
-export default function GithubProjects() {
-  const fetcher: Fetcher<{ data?: IData }, string> = (args) =>
-    fetch(args).then(async (res) => {
+const GithubProjects: React.FunctionComponent = () => {
+  const fetcher: Fetcher<{ data?: IData }, string> = args =>
+    fetch(args).then(async res => {
       if (res.ok) return res.json();
       throw res.statusText;
     });
-  const { data, error } = useSWR("/api/repositories", fetcher);
+  const { data, error } = useSWR('/api/repositories', fetcher);
   const isLoading = !data && !error;
 
   const pinnedRepositories = data?.data?.viewer?.pinnedItems?.nodes ?? [];
-  const latestRepositories =
-    data?.data?.viewer?.repositories?.edges?.map((edge) => edge.node) ?? [];
+  const latestRepositories = data?.data?.viewer?.repositories?.edges?.map(edge => edge.node) ?? [];
 
   return (
     <>
@@ -39,42 +38,26 @@ export default function GithubProjects() {
         {error ? (
           <Card className="mx-auto my-8 max-w-screen-md">
             <h3 className="mb-2 text-center text-3xl">Error</h3>
-            <p className="mb-4 text-center font-bold">
-              Please contact the developer with the following detail
-            </p>
+            <p className="mb-4 text-center font-bold">Please contact the developer with the following detail</p>
             <p className="text-center">{error}</p>
           </Card>
         ) : (
           <>
-            <h3 className="mt-8 mb-4 text-center text-3xl">
-              Pinned Repositories
-            </h3>
+            <h3 className="mt-8 mb-4 text-center text-3xl">Pinned Repositories</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
               {isLoading
-                ? Array.from({ length: 6 }).map((_, idx) => (
-                    <Skeleton className="mx-auto h-32 w-full" key={idx} />
-                  ))
+                ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
                 : pinnedRepositories.map((repository, idx) => (
-                    <GithubProjectsRepositoryCard
-                      key={idx}
-                      repository={repository}
-                    />
+                    <GithubProjectsRepositoryCard key={idx} repository={repository} />
                   ))}
             </div>
 
-            <h3 className="mt-8 mb-4 text-center text-3xl">
-              Latest Repositories
-            </h3>
+            <h3 className="mt-8 mb-4 text-center text-3xl">Latest Repositories</h3>
             <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3">
               {isLoading
-                ? Array.from({ length: 6 }).map((_, idx) => (
-                    <Skeleton className="mx-auto h-32 w-full" key={idx} />
-                  ))
+                ? Array.from({ length: 6 }).map((_, idx) => <Skeleton className="mx-auto h-32 w-full" key={idx} />)
                 : latestRepositories.map((repository, idx) => (
-                    <GithubProjectsRepositoryCard
-                      key={idx}
-                      repository={repository}
-                    />
+                    <GithubProjectsRepositoryCard key={idx} repository={repository} />
                   ))}
             </div>
           </>
@@ -82,4 +65,6 @@ export default function GithubProjects() {
       </section>
     </>
   );
-}
+};
+
+export default GithubProjects;
