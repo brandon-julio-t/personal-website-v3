fb2817b4b430326cb3b1bab2620755ec797c72fa
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-02-18 07:30:11 +0700

    fix: 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,9 +1,13 @@
-import * as React from "react";
+import * as React from 'react';
 
 export default () => {
   return (
-    <h1 className="h-screen w-screen text-center text-9xl">
-      404 <br /> Not Found
-    </h1>
+    <div className="flex h-screen items-center justify-center">
+      <h1 className="text-center text-9xl">
+        <span className="bg-gradient-to-r from-indigo-500 to-purple-500 box-decoration-clone bg-clip-text text-transparent">
+          404 <br /> Not Found
+        </span>
+      </h1>
+    </div>
   );
 };
