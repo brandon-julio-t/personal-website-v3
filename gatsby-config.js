55563405602478d7c06f36748cc61423c5fcba97
Brandon Julio Thenaro <brandon.julio.t@icloud.com>
2022-02-18 07:12:02 +0700

    feat: gatsby cloud plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,7 @@ module.exports = {
   },
   plugins: [
     "gatsby-plugin-fontawesome-css",
+    "gatsby-plugin-gatsby-cloud",
     "gatsby-plugin-image",
     "gatsby-plugin-offline",
     "gatsby-plugin-postcss",
